c177dcc09d37454faaf1e6e3882b1447f8edf0d9
Xerilious <trevorflanagan2001@gmail.com>
2018-09-11 09:18:23 -0700

    adding project

---
+++ Unit2Alab/Unit2Alab.py
@@ -0,0 +1,8 @@
+i=100
+j=200
+x=300
+y=100
+print ((i==j) or (i>j))
+print ((i<j) and (i>=y))
+print ((i!=j) and (i!=x))
+print ((i<j) and (i<x) and (i<y))
---
+++ Unit2Blab/Unit2Blab.py
@@ -0,0 +1,13 @@
+numGrade = int(input())
+if numGrade >= 90:
+    print ('Yahoo you got an A')
+elif numGrade >= 80:
+    print ('Good you got a B')
+elif numGrade >= 70:
+    print ('Okay you got a C')
+
+userInut = int(input())
+if userInut == blue:
+    print ('The sky is blue')
+elif userInut == green:
+    print ('The forest is green')
