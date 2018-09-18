71247984d72e8d4b04c10d74f54e4c75896cfbc8
Xerilious <trevorflanagan2001@gmail.com>
2018-09-18 09:21:45 -0700

    h

---
+++ Unit3lab/Unit3lab.py
@@ -0,0 +1,12 @@
+def schoolsubject():
+    print('Bellarmine Prep')
+    print('Favorite Subject Is Python')
+schoolsubject()
+
+def yearsInSchool(grade):
+    (int(grade))
+    years=grade+1
+    print('My Grade Is -')+str(yearsInSchool)
+myGrade =input('Enter Your Grade - ')
+
+yearsInSchool()
