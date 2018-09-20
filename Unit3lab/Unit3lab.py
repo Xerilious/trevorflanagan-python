c7d8027dada314faaf62955469ef3d20d7db31af
Xerilious <trevorflanagan2001@gmail.com>
2018-09-20 09:19:16 -0700

    Hurray

--- Unit3lab/Unit3lab.py
+++ Unit3lab/Unit3lab.py
@@ -3,10 +3,22 @@ def schoolsubject():
     print('Favorite Subject Is Python')
 schoolsubject()
 
-def yearsInSchool(grade):
-    (int(grade))
-    years=grade+1
-    print('My Grade Is -')+str(yearsInSchool)
-myGrade =input('Enter Your Grade - ')
 
-yearsInSchool()
+def yearsInSchool(grade):
+    grade=int(grade)
+
+
+myGrade = input('Enter Your Grade - ')
+myGrade = int(myGrade)+1
+print('Your Years In School Are - ' + str(myGrade))
+yearsInSchool(grade=myGrade)
+
+def gradeAndCity(city, grade):
+    x = grade - 1
+    print('Good Morning ' + city)
+    print('You Are In ' + str(x) + 'th Grade')
+
+
+MyCity = input('Where Are You From? - ')
+MyGrade = input('How Many Years Have You Been In School? - ')
+gradeAndCity(MyCity, int(MyGrade))
