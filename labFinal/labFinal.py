2afc8ab69c5e5f5a4af67a133fbb1a0462c0ca4d
Xerilious <trevorflanagan2001@gmail.com>
2018-09-28 09:26:32 -0700

    looiking better

--- labFinal/labFinal.py
+++ labFinal/labFinal.py
@@ -2,17 +2,9 @@ def main():
     longYear()
     num1 = [86.5, 75.3, 98.9, 92.3]
     num2 = len(num1)
+    print(num1)
     numGradeList(num1, num2)
-    avgGrade = 
-    if avgGrade >= 90:
-        print('You Are Passing!')
-    elif avgGrade >= 80:
-        print('You Are Passing!')
-    elif avgGrade >= 70:
-        print('Barely Passing!')
-    else:
-        print('Start Working!')
-    gradeCheck()
+    gradeAvg = gradeCheck()
 
 def longYear():
     shortYear = int(input('Type In Year - '))
@@ -26,13 +18,20 @@ def longYear():
         print ('Senior')
 
 def numGradeList(myNum1, myNum2):
-    print(myNum1)
     gradeAvg = (myNum1[0] + myNum1[1] + myNum1[2] + myNum1[3])/myNum2
-    return gradeAvg
+    return (gradeAvg)
 
 
 def gradeCheck():
-
+    avgGrade =
+    if avgGrade >= 90:
+        print('You Are Passing!')
+    elif avgGrade >= 80:
+        print('You Are Passing!')
+    elif avgGrade >= 70:
+        print('Barely Passing!')
+    else:
+        print('Start Working!')
 
 
 
