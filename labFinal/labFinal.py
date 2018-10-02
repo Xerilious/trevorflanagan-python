47cf85beb2d49a8625beee465d680963f4d219ba
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-10-02 09:36:13 -0700

    working

--- labFinal/labFinal.py
+++ labFinal/labFinal.py
@@ -1,37 +1,49 @@
 def main():
     longYear()
-    num1 = [86.5, 75.3, 98.9, 92.3]
+    num1 = [50, 70, 50.3, 20.7]
     num2 = len(num1)
     print(num1)
-    numGradeList(num1, num2)
-    gradeAvg = gradeCheck()
+    answer = numGradeList(num1, num2)
+    process = gradeCheck(answer)
+    gradeResponse(process)
 
 def longYear():
     shortYear = int(input('Type In Year - '))
-    if shortYear < 10:
+    if shortYear == 9:
         print ('Freshmen')
-    elif shortYear < 11:
+    elif shortYear == 10:
         print ('Sophmore')
-    elif shortYear < 12:
+    elif shortYear == 11:
         print ('Junior')
-    elif shortYear < 13:
+    elif shortYear == 12:
         print ('Senior')
+    else:
+        print('You Should Not Be Here')
 
 def numGradeList(myNum1, myNum2):
     gradeAvg = (myNum1[0] + myNum1[1] + myNum1[2] + myNum1[3])/myNum2
-    return (gradeAvg)
+    return gradeAvg
 
 
-def gradeCheck():
-    avgGrade =
-    if avgGrade >= 90:
+def gradeCheck(gradeAvg):
+    if gradeAvg >= 90:
+        print('A')
+    elif gradeAvg >= 80:
+        print('B')
+    elif gradeAvg >= 70:
+        print('C')
+    else:
+        print('Uh Oh')
+    return gradeCheck
+
+
+def gradeResponse(gradeCheck):
+    print(gradeCheck)
+    if gradeCheck == 'A' or 'B' or 'C':
         print('You Are Passing!')
-    elif avgGrade >= 80:
-        print('You Are Passing!')
-    elif avgGrade >= 70:
-        print('Barely Passing!')
     else:
         print('Start Working!')
+    return gradeCheck
 
 
 
