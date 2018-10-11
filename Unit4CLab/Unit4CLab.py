1d51bfbd85d6989d153e52e190470194a8b40722
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-10-11 09:20:56 -0700

    wip

---
+++ Unit4CLab/Unit4CLab.py
@@ -0,0 +1,15 @@
+def mainFunc():
+    drawSev()
+
+
+def drawSev():
+    sevenNums = ''
+    sevNums = 'x'
+    for x in sevNums:
+        for y in range(0,8):
+            sevenNums += ' * '
+            print(sevenNums)
+
+
+
+mainFunc()
