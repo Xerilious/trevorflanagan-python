0b167c4d8857d429abf14a3f2ed2c408b696eabf
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-10-15 09:14:31 -0700

    wip

--- Unit4CLab/Unit4CLab.py
+++ Unit4CLab/Unit4CLab.py
@@ -1,6 +1,7 @@
 def mainFunc():
     drawSev()
     starsAndStripes()
+    incTri()
 
 
 def drawSev():
@@ -8,16 +9,25 @@ def drawSev():
         for y in range(0,7):
             print('* ',end='')
         print()
+    print()
 
 
 def starsAndStripes():
-    for x in range(0,3):
-        for y in range(0,7):
-           print('*',end='')
+    for x in range(0, 3):
+        for y in range(0, 7):
+            print('* ', end='')
+        print('')
         for z in range(0,7):
-           print('-',end='')
+            print('- ', end='')
         print()
 
+def incTri():
+    z = 0
+    for x in range(0, 7):
+        z = z + 1
+        print('')
+        for y in range(0, z):
+            print(z, end='')
 
 
 mainFunc()
