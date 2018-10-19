3fbc3379664de697f78ad57db9b92e04ef7b7dc9
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-10-19 08:11:43 -0700

    wip

--- Unit4BLab/Unit4BLab.py
+++ Unit4BLab/Unit4BLab.py
@@ -1,6 +1,7 @@
 def main():
     loopList = [1, 2, 3, 4, 5]
     forLoopPrac(loopList)
+    loopWithRange()
 
 
 def forLoopPrac(loopList):
@@ -11,7 +12,9 @@ def forLoopPrac(loopList):
 
 def loopWithRange():
     rangeList = [10, 20, 30, 40, 50]
-    
+    for x in range(0, 3):
+            print(rangeList[x])
+
 
 
 main()
