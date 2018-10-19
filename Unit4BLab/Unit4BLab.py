d25cd43cdc3b11d1490b2f9dbe9dc385b327d746
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-10-19 08:53:52 -0700

    Done!

--- Unit4BLab/Unit4BLab.py
+++ Unit4BLab/Unit4BLab.py
@@ -7,13 +7,15 @@ def main():
 def forLoopPrac(loopList):
     for x in loopList:
         print(x, '+ 10 =', (x + 10))
-        print(x, '* 10 =', (x + 10))
+        print(x, '* 10 =', (x * 10))
 
 
 def loopWithRange():
+    #rangeListAfter = ''
     rangeList = [10, 20, 30, 40, 50]
-    for x in rangeList:
-            print(x, '* 10 =', (x * 10))
+    for x in range(0, len(rangeList)):
+            rangeList[x] = rangeList[x] * 10
+    print(rangeList)
 
 
 
