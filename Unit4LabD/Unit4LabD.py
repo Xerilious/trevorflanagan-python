f215aa0f475609ee9136c1d95168aadbb1444c87
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-10-23 09:37:13 -0700

    whoops

--- Unit4LabD/Unit4LabD.py
+++ Unit4LabD/Unit4LabD.py
@@ -1,1 +1,17 @@
 def main():
+    allInOne()
+
+
+def allInOne():
+    revisedCart = []
+    shoppingCart = [['toothpaste', 'q-tips', 'milk'], ['milk', 'candy', 'apples'], ['paper', 'pencils', 'q-tips']]
+    for subList in shoppingCart:
+        #print(subList)
+        for item in subList:
+            #print(item)
+            if item not in revisedCart:
+                revisedCart.append(item)
+    print(revisedCart)
+
+
+main()
