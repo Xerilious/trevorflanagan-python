9c9b5a0f4523eaebd2bf5eca2dade2b82a7394ec
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-10-25 08:34:30 -0700

    whoops

--- Unit4LabD/Unit4LabD.py
+++ Unit4LabD/Unit4LabD.py
@@ -1,17 +1,27 @@
 def main():
     allInOne()
+    print(shoppingCart)
+    countQTips()
 
 
 def allInOne():
     revisedCart = []
     shoppingCart = [['toothpaste', 'q-tips', 'milk'], ['milk', 'candy', 'apples'], ['paper', 'pencils', 'q-tips']]
     for subList in shoppingCart:
-        #print(subList)
         for item in subList:
-            #print(item)
             if item not in revisedCart:
                 revisedCart.append(item)
     print(revisedCart)
+    return shoppingCart
+
+
+def countQTips(shoppingCart):
+    onlyQTips = []
+    for sublist in shoppingCart:
+        for qTipNum in sublist:
+            if qTipNum in onlyQTips:
+                onlyQTips.append(qTipNum)
+    print(onlyQTips)
 
 
 main()
