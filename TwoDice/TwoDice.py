869c56e61853cab88124e72bf4ec751509d5e4d5
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-11-08 08:06:54 -0800

    wip

--- TwoDice/TwoDice.py
+++ TwoDice/TwoDice.py
@@ -3,37 +3,45 @@ ROLLNUM = 2
 
 
 def mainFunc():
-    answer = findDice()
-    defineDice(answer)
-    print(answer)
+    setOfDice = [0] * 6
+    #setOfDice = defineDice(num)
+    num = findDice()
+    defineDice(num)
+    setOfDice = defineDice(num)
+    print(num)
     sideBySide()
 
 
-def defineDice(answer):
+def defineDice(num):
+    dice = [0] * 6
     topBottom = ' ~~~~~ '
     leftOne =   '|@    |'
     middleOne = '|  @  |'
     rightOne =  '|    @|'
     aCouple =   '|@   @|'
     emptyOne =  '|     |'
-    #if answer == 'snakeeyes!':
-    #print(answer)
-        if answer == 1:
-            print(topBottom, emptyOne, middleOne, emptyOne, topBottom)
-        if answer == 2:
-            print(topBottom, leftOne, emptyOne, rightOne, topBottom)
-        if answer == 3:
-            print(topBottom, leftOne, middleOne, rightOne, topBottom)
-        if answer == 4:
-            print(topBottom, aCouple, emptyOne, aCouple, topBottom)
-        if answer == 5:
-            print(topBottom, leftOne, emptyOne, rightOne, topBottom)
+    for num in range(0, 6):
+        if num == 0:
+            dice[num] = [topBottom, emptyOne, middleOne, emptyOne, topBottom]
+        elif dice[num] == 1:
+            dice[num] = [topBottom, leftOne, emptyOne, rightOne, topBottom]
+        elif dice[num] == 2:
+            dice[num] = [topBottom, leftOne, middleOne, rightOne, topBottom]
+        elif dice[num] == 3:
+            dice[num] = [topBottom, aCouple, emptyOne, aCouple, topBottom]
+        elif dice[num] == 4:
+            dice[num] = [topBottom, leftOne, emptyOne, rightOne, topBottom]
+        else:
+            dice[num] = [topBottom, aCouple, aCouple, aCouple, topBottom]
+    print(dice)
+
+    return dice
 
 
 
 def findDice():
-    rollDice = randint(1, 6)
-    return rollDice
+    num = randint(1, 6)
+    return num
 
 
 def sideBySide():
