210c0eb34d88d7e5aa293a0166e97dbe2e392224
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-11-26 08:55:38 -0800

    yay

--- TurtleLab4/TurtleLab4.py
+++ TurtleLab4/TurtleLab4.py
@@ -3,16 +3,30 @@ pen = turtle.Pen()
 pen.speed(0)
 def mainFunc():
     creativeThingy()
+    rosetteThingy()
 
 
 def creativeThingy():
-    for x in range(0, 800):
+    for x in range(0, 50):
         pen.pencolor('red')
         pen.forward(100)
         pen.left(100)
         pen.pencolor('blue')
         pen.forward(100)
-        pen.left(99)
+        pen.left(168)
+
+
+def rosetteThingy():
+    for x in range(0, 119):
+        pen.pencolor('#748ff9')
+        pen.circle(50)
+        pen.left(47)
+        pen.pencolor('#f928f2')
+        pen.circle(100)
+        pen.left(87)
+        pen.pencolor('#ff1429')
+        pen.circle(25)
+
 
 
 mainFunc()
