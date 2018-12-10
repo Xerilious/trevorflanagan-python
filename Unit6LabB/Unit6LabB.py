af3a843df5ff43ce7c5bb7ae4d054271a7ed1148
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-12-10 08:29:08 -0800

    hurrah

--- Unit6LabB/Unit6LabB.py
+++ Unit6LabB/Unit6LabB.py
@@ -1,13 +1,12 @@
 def mainFunc():
     weekendFunc()
 
-#flag = False
 
 def weekendFunc():
     flag = False
     weekends = {'Jan': [6,7,13,14,20,21,27,28], 'Feb': [3,4,10,11,17,18,24,25], 'Mar': [3,4,10,11,17,18,24,25,31], 'Apr': [1,7,8,14,15,21,22,28,29], 'May': [5,6,12,13,19,20,26,27], 'Jun': [2,3,9,10,16,17,23,24,30], 'Jul': [1,7,8,14,15,21,22,28,29], 'Aug': [4,5,11,12,18,19,25,26], 'Sep': [1,2,8,9,15,16,22,23,29,30], 'Oct': [6,7,13,14,20,21,27,28], 'Nov': [3,4,10,11,17,18,24,25], 'Dec': [1,2,8,9,15,16,22,23,29,30]}
     month = input('Birth Month Abbreviation: - ')
-    date = input('Birth Date: - ')
+    date = int(input('Birth Date: - '))
     if month in weekends:
         print(month)
         if date in weekends[month]:
@@ -17,9 +16,12 @@ def weekendFunc():
     if flag == True:
         print('weekend')
 
+    if flag != True:
+        print('Too Bad')
+
     #else:
         #print('Not A Weekend')
-    print(month + '/' + date + '/2018')
+    print(month + '/' + str(date) + '/2018')
 
 
 
