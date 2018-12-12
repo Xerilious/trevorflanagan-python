1ab26bee4f805057274ae538f0c069c65ee96c5a
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2018-12-12 09:20:25 -0800

    gad dammmmmmmiiiiiittttt

--- Unit7LabA/Unit7LabA.py
+++ Unit7LabA/Unit7LabA.py
@@ -1,5 +1,35 @@
 def mainFunc():
-    
+    print('Gad Dammit')
+
+
+class petClass:
+    petType = 'Cage Free Pet'
+
+    def __init__(self, vType, vName, vBreed):
+        self.type = vType
+        self.name = vName
+        self.breed = vBreed
+    def getType(self):
+        return(str(self.type))
+
+    def getName(self):
+        return(str(self.name))
+
+    def getbreed(self):
+        return(str(self.breed))
+
+    def whatIsIt(self):
+        print(petClass)
+
+class cageClass:
+    cageClass = 'Caged Pet'
+
+    def __init__(self, vType, vName, vBreed):
+        self.type = 
+        self.name = vName
+        self.breed = vBreed
+    def dangerThingy(self):
+
 
 
 
