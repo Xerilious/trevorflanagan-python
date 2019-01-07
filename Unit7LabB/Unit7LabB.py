ced842a22b8dfe4ea4ca225e414e06525c98e283
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2019-01-07 09:02:50 -0800

    hurrah!

--- Unit7LabB/Unit7LabB.py
+++ Unit7LabB/Unit7LabB.py
@@ -1,17 +1,39 @@
 def mainFunc():
     print('here')
+    myPet1 = pet('Jemma', 'Australian Labradoodle')
+    print(myPet1.name)
+    print(myPet1.breed)
+    myPet2 = dog('Loki', 'Australian Labradoodle', 'Tan')
+    print(myPet2.name)
+    print(myPet2.breed)
+    print(myPet2.color)
+    myPet1.whatIsIt()
+    myPet2.whyIsIt()
 
-class petClass:
-    petType = 'Cage Free Pet'
 
-    def __init__(self, vType, vName, vBreed):
-        self.type = vType
+class pet:
+
+    def __init__(self, vName, vBreed):
         self.name = vName
         self.breed = vBreed
 
     def whatIsIt(self):
-        print('My pet is a ' + self.type + ', his name is ' + self.name + ', and he is a ' + self.breed)
+        print("My pet's name is " + self.name + ', and he is a ' + self.breed)
 
 
+class dog(pet):
+    def __init__(self, vName, vBreed, vColor):
+        pet.__init__(self, vName, vBreed)
+        self.color = vColor
 
-    mainFunc()
+
+    def whyIsIt(self):
+        print("My pet's name is " + self.name + ', and he is a ' + self.breed + ', he is ' + self.color)
+
+    #def __init__(self, vName, vBreed):
+        #parentClass.__init__(self, vName, vBreed)
+        #self.name = vName
+        #self.breed = vBreed
+
+
+mainFunc()
