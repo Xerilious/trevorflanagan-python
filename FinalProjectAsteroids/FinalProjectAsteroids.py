72dd73803c56246cb4808ceaf7509aeb60e50719
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2019-01-09 10:09:02 -0800

    skdjhfkuhsdkgfkjds

--- FinalProjectAsteroids/FinalProjectAsteroids.py
+++ FinalProjectAsteroids/FinalProjectAsteroids.py
@@ -1,22 +1,15 @@
-WIDTH = 800
-HEIGHT = 600
+#Etch-A-Sketch!
+import turtle
+import random
+pen = turtle.Pen()
+pen.speed(0)
+def mainFunc():
+    pen.pencolor('red', 'yellow')
+while True:
+    pen.forward(200)
+    pen.pencolor(random.randint(0, 255)/255, random.randint(0, 255)/255, random.randint(0, 255)/255)
+    pen.left(178)
 
-center = WIDTH / 2, HEIGHT / 2
-circle_image = 'circle'
-left_arrow_image = 'left-arrow'
-right_arrow_image = 'right-arrow'
-current_actor = Actor(circle_image)
-current_actor.pos = center
 
-def draw():
-    screen.fill((128, 0, 0))
-    current_actor.draw()
-
-def update():
-
-    if keyboard.left:
-        current_actor.image = left_arrow_image
-    elif keyboard.right:
-        current_actor.image = right_arrow_image
-    else:
-        current_actor.image = circle_image
+mainFunc()
+turtle.exitonclick
