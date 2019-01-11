0e5b38d3ff1127ad2fd113cb8e11d947413e1114
trevorflanagan-python <Trevorflanagan2081@gmail.com>
2019-01-11 09:21:00 -0800

    skdjhfkuhsdkgfkjds

--- FinalProjectAsteroids/FinalProjectAsteroids.py
+++ FinalProjectAsteroids/FinalProjectAsteroids.py
@@ -2,14 +2,27 @@
 import turtle
 import random
 pen = turtle.Pen()
+pen1 = turtle.Pen()
 pen.speed(0)
+pen1.speed(0)
+antonia = turtle.Screen()
+wn = turtle.Screen()
 def mainFunc():
     pen.pencolor('red', 'yellow')
+    jimBurden()
+
+
+def jimBurden():
+    pen.forward(10)
+
+
 while True:
     pen.forward(200)
     pen.pencolor(random.randint(0, 255)/255, random.randint(0, 255)/255, random.randint(0, 255)/255)
     pen.left(178)
+    antonia.onkey(jimBurden(), 'Up')
+    wn.onkeypress(print('right'))
 
 
 mainFunc()
-turtle.exitonclick
+#turtle.exitonclick
